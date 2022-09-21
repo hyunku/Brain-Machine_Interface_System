1e8fb457fa76b527d710de3305030ede57122bf2
hyunku <kahk000@naver.com>
2022-09-22 04:47:33 +0900

    Update decoder_2.py

--- src/decoder_2.py
+++ src/decoder_2.py
@@ -8,6 +8,7 @@ from config import load_parameters
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from SimpleESN import SimpleESN
 
 # parameters
 device = 'cpu' # for pytorch inference
